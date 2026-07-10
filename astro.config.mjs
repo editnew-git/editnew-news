2980c7b39a2963a75f751497256ad1d684a12261
editnew-git <editnew.ca>
2026-07-10 10:39:29 -0700

    feat: refresh Editnew resource center

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
-  site: 'https://news.editnew.ca',
+  site: 'https://support.editnew.ca',
 });
